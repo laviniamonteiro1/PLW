ac17de680d8665eee5e69999b96714f630d3b779
laviniamonteiro1 <laviniamonteiro10@gmail.com>
2025-06-16 15:42:40 -0300

    ultima

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,7 @@
-/// <reference types="vitest/config" /> 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths' // ✅ Import do plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths() // ✅ Adicionado para suportar `@/`
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts'
-  }
+  plugins: [react()],
 })
